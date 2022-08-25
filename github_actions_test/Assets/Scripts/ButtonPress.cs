7667d2ac99259e269cd2946959c9aefc1d53ae3d
Jody van der Byl <jody@renderheads.com>
2022-08-25 14:14:01 +0200

    Unity Scene

---
+++ github_actions_test/Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPress : MonoBehaviour
+{
+
+    [SerializeField] private string[] indexedText;
+    [SerializeField] private Text editableText;
+
+    private int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        editableText.text = indexedText[0];
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonPress()
+    {
+        if (counter < 5)
+        {
+            counter++;
+        }
+        else
+        {
+            counter = 0;
+        }
+
+        editableText.text = indexedText[counter];
+    }
+}
